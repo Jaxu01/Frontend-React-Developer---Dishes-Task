3c8c577459af0e9a5ae6ec0c25f0cdc85988c3d9
Jakub Piwtorak <jakubpiwtorak@gmail.com>
2023-05-29 00:24:16 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Form from './Form.tsx'
 import { FormResponse } from './types.ts'
-import './App.css'
 
 const App = () => {
   const [formResponse, setFormResponse] = useState<FormResponse|null>(null)
@@ -12,8 +11,11 @@ const App = () => {
       <h1>Dishes Task</h1>
       <div className="paper">
         {formResponse ? (
-          <div style={{color: 'black'}}>{formResponse.name} Form Sent
-            <button onClick={() => setFormResponse(null)}>Send Another</button>
+          <div className="text-confirmation">{formResponse.name} Form Sent
+          <div className="checkmark-wrapper">
+             <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/> <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+          </div>
+            <button className="send-another" onClick={() => setFormResponse(null)}>Send Another</button>
           </div>
           )
           : (
