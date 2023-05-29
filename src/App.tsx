ee3d0ba6e0f28814cbe9aee6e9329d6912ac8dc3
Jakub Piwtorak <jakubpiwtorak@gmail.com>
2023-05-29 18:17:10 +0200

    Final Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@ import { FormResponse } from './types.ts'
 
 const App = () => {
   const [formResponse, setFormResponse] = useState<FormResponse|null>(null)
-  console.log(formResponse)
   
   return (
     <>
       <h1>Dishes Task</h1>
       <div className="paper">
         {formResponse ? (
-          <div className="text-confirmation">{formResponse.name} Dish Sent
-          <div className="checkmark-wrapper">
-             <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/> <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
-          </div>
-            <button className="send-another" onClick={() => setFormResponse(null)}>Send Another</button>
-          </div>
-          )
-          : (
-            <Form setFormResponse={setFormResponse}></Form>
+            <div className="text-confirmation">{formResponse.name} Dish Sent
+              <div className="checkmark-wrapper">
+                <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/> <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+              </div>
+              <button className="send-another" onClick={() => setFormResponse(null)}>Send Another</button>
+            </div>
+          ) : (
+            <Form setFormResponse={setFormResponse}/>
           )
         }
       </div>
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,98 +1,98 @@
 import { useState, FormEvent } from 'react'
 
 const Form = ({setFormResponse}) => {
-    const [dish, setDish] = useState<string>("")
-  
-    const postData = async(data: object) => {
-      const url = "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/"
-      
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-      
-      return response.json()
-    }
-  
-    const formatNumbers = (data: object) => {
-      for (const property in data) {
-        const value = data[property]
-        const valueNumber = Number(value)
-        const isNumber = !!valueNumber
-        const isFloat = isNumber && value.includes(".")
-        if (isFloat) {
-          data[property] = valueNumber
-        } else if (isNumber) {
-          data[property] = valueNumber
-        }
-      }
-      return data
-    }
-  
-    const sendData = async(event: FormEvent) => {
-      if (event.target instanceof HTMLFormElement) {
-        const formElement:HTMLFormElement = event.target
-        event.preventDefault()
-        const formData = new FormData(formElement)
-        const formObject = Object.fromEntries(formData)
-        const formatedData = formatNumbers(formObject)
-        const response = await postData(formatedData)
-        setFormResponse(response)
+  const [dish, setDish] = useState<string>("")
+
+  const postData = async(data: object) => {
+    const url = "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/"
+    
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    
+    return response.json()
+  }
+
+  const formatNumbers = (data: object) => {
+    for (const property in data) {
+      const value = data[property]
+      const valueNumber = Number(value)
+      const isNumber = !!valueNumber
+      const isFloat = isNumber && value.includes(".")
+      if (isFloat) {
+        data[property] = valueNumber
+      } else if (isNumber) {
+        data[property] = valueNumber
       }
     }
-    return (
-        <form onSubmit={sendData}>
-          <label className="select-label">Select a Dish</label>
-          <div className="dish-selection">
-            <select className="input-class" name="type" onChange={({target}) => setDish(target.value)}>
-              <option hidden> -- Select an Option -- </option>
-              <option>pizza</option>
-              <option>soup</option>
-              <option>sandwich</option>
-            </select>
-          </div>
-          <div className="wrapper">
-          <fieldset>
-            <label className="input-label">Dish Name</label>
-              <input className="dish-name input-class" type="text" name="name" placeholder="what dish?" required={true}/>
-          </fieldset>
-          <fieldset>
-            <label className="input-label">Preparation Time</label>
-              <input className="prep-time input-class" name="preparation_time" type="time" step="1" required={true}/>
-          </fieldset>
-          {dish === "pizza" && (
-            <>
-              <fieldset className="pizza-choices">
-                <label className="input-label">How many slices?</label>
-                <input className="slices input-class" name="no_of_slices" placeholder="number of slices" type="number" required={true}/>
-              </fieldset>
-              <fieldset className="pizza-choices">
-                <label className="input-label">What is the diameter?</label>
-                <input className="diameter input-class" name="diameter" onBlur={({target}) => target.value = Number(target.value).toFixed(1)} min="0" placeholder="diameter of the pizza" step="0.1" type="number" required={true}/>
-              </fieldset>
-            </>
-          )}
-          {dish === "soup" && (
+    return data
+  }
+
+  const sendData = async(event: FormEvent) => {
+    if (event.target instanceof HTMLFormElement) {
+      const formElement:HTMLFormElement = event.target
+      event.preventDefault()
+      const formData = new FormData(formElement)
+      const formObject = Object.fromEntries(formData)
+      const formatedData = formatNumbers(formObject)
+      const response = await postData(formatedData)
+      setFormResponse(response)
+    }
+  }
+  return (
+    <form onSubmit={sendData}>
+      <label className="select-label">Select a Dish</label>
+      <div className="dish-selection">
+        <select className="input-class" name="type" onChange={({target}) => setDish(target.value)}>
+          <option hidden> -- Select an Option -- </option>
+          <option>pizza</option>
+          <option>soup</option>
+          <option>sandwich</option>
+        </select>
+        </div>
+        <div className="wrapper">
+        <fieldset>
+          <label className="input-label">Dish Name</label>
+          <input className="dish-name input-class" type="text" name="name" placeholder="what dish?" required={true}/>
+        </fieldset>
+        <fieldset>
+          <label className="input-label">Preparation Time</label>
+          <input className="prep-time input-class" name="preparation_time" type="time" step="1" required={true}/>
+        </fieldset>
+        {dish === "pizza" && (
+          <>
+            <fieldset className="pizza-choices">
+              <label className="input-label">How many slices?</label>
+              <input className="slices input-class" name="no_of_slices" placeholder="number of slices" type="number" required={true}/>
+            </fieldset>
+            <fieldset className="pizza-choices">
+              <label className="input-label">What is the diameter?</label>
+              <input className="diameter input-class" name="diameter" onBlur={({target}) => target.value = Number(target.value).toFixed(1)} min="0" placeholder="diameter of the pizza" step="0.1" type="number" required={true}/>
+            </fieldset>
+          </>
+        )}
+        {dish === "soup" && (
           <div className="soup-choices">
             <label className="input-label">How spicy is it?</label>
             <input className="spice-scale input-class" name="spiciness_scale" placeholder="spiciness scale" min="1" max="10" type="number" required={true}/>
           </div>
-          )}
-          {dish === "sandwich" && (
+        )}
+        {dish === "sandwich" && (
           <div className="sandwich-choices">
             <label className="input-label">How many slices?</label>
             <input className="bread-slices input-class" name="slices_of_bread" placeholder="slices of bread" type="number" required={true}/>
           </div>
-          )}
+        )}
         <div className="button">
           <input type="submit" name="" value="Send"/>
         </div>
-        </div>
-      </form>
-    )
+      </div>
+    </form>
+  )
 }
 
 export default Form
